8aaf39bc26748be147d3a4d896f70eaed424ca46
Daniel Szaniszlo <daniel.szaniszlo@gmail.com>
2023-02-17 15:58:34 -0800

    Update prompt

--- netlify/functions/artistic-preference.ts
+++ netlify/functions/artistic-preference.ts
@@ -15,11 +15,11 @@ export interface ImageMetadata {
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
   try {
     const payload = JSON.parse(event.body!) as ImageMetadata;
-
+    const prompt = `I want you to act like an artist and your imagination. I'll give you the details to create a creative description for your latest art. Use the following style/styles: ${payload.style} use the following technique/techniques: ${payload.technique} and the subject/subjects of the art should be ${payload.subject}. Make the description in 2 sentences.`;
     const completion = await openai.createCompletion({
       model: 'text-davinci-003',
       max_tokens: 150,
-      prompt: `Make an image description from the following properties. style: ${payload.style} subject: ${payload.subject} and technique: ${payload.technique}`,
+      prompt,
       temperature: 0.6,
     });
     const imgDescription = completion.data.choices[0].text ?? '';
